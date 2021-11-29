515b15abeb780324b23fe34a9ab39ea30ca90077
iSebastianCabrera <jscrojas25@yahoo.es>
2021-11-29 16:38:58 -0500

    creando entidades de account y movimientos con metodos

---
+++ src/main/java/Movimiento.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+
+public class Movimiento {
+
+    private LocalDate fecha;
+
+    private double balance;
+    private double deposito;
+    private double retiro;
+    private double transaccion;
+
+    public Movimiento(LocalDate fecha, double balance, String operacion, double deposito) {
+        this.balance = balance;
+        this.fecha = fecha;
+        Operacion(operacion,deposito);
+    }
+
+    public void Operacion(String operacion,double deposito){
+        switch (operacion){
+            case "deposito":this.deposito = deposito;break;
+            case "retiro":this.retiro = deposito;break;
+            default:this.transaccion = deposito;break;
+        }
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public double getRetiro() {
+        return retiro;
+    }
+}
